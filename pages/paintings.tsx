b35aaf01868678fc545cdd9d97af85606da4bf9f
rashodk <rmanamperiko@mun.ca>
2023-01-22 01:19:17 -0330

    corrected Navbar file delecration

--- pages/paintings.tsx
+++ pages/paintings.tsx
@@ -1,7 +1,9 @@
 import Footer from "../src/Components/Footer";
 // import Navbar from "../src/Components/navbar";
 import Gallery from "../src/Components/Gallery";
-import Navbar from "../src/Components/navbar";
+
+import Navbar from "../src/Components/NavBar";
+
 export default function Paintings() {
   return (
     <div>
