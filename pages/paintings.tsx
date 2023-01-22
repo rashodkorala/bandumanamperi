b54944f93f511ab9a3543bcf3aa8d3f158cd2afe
rashodk <rmanamperiko@mun.ca>
2023-01-22 01:13:51 -0330

    added navbar

--- pages/paintings.tsx
+++ pages/paintings.tsx
@@ -1,11 +1,11 @@
 import Footer from "../src/Components/Footer";
 // import Navbar from "../src/Components/navbar";
 import Gallery from "../src/Components/Gallery";
-
+import Navbar from "../src/Components/navbar";
 export default function Paintings() {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Gallery />
       <Footer />
     </div>
