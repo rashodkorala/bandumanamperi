0aab478dfbf2ba462fada2fbdcf49b7f00873658
rashodk <rmanamperiko@mun.ca>
2023-01-22 01:04:46 -0330

    replace <a> with <Link>

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import {
   FaFacebook,
@@ -6,6 +7,7 @@ import {
   FaTwitter,
 } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
+
 type Props = {};
 
 const Footer = (props: Props) => {
@@ -14,7 +16,6 @@ const Footer = (props: Props) => {
       id="Footer"
       className=" bg-black flex flex-col w-full h-[500px] md:h-[300px] bottom-0 text-[#ffffff7d] justify-center items-staet text-[12px]md:text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px]"
     >
-      
       <div className="p-3">
         <div className="py-3">Bandu Manamperi.</div>
         <div className="grid grid-cols-2 justify-start items-start gap-7 md:grid-cols-3">
@@ -31,27 +32,27 @@ const Footer = (props: Props) => {
             <h2 className="text-[14px] font-bold pt-1">SOCIAL</h2>
             <ul className="flex flex-col text-[14px]">
               <li className="flex py-1 px-0 items-center">
-                <a href="/">
+                <Link href="/">
                   <FaFacebook size={15} className=" mr-1" />
-                </a>
+                </Link>
                 Facebook
               </li>
               <li className="flex py-1 px-0 items-center">
-                <a href="/">
+                <Link href="/">
                   <FaInstagram size={15} className="mr-1" />
-                </a>
+                </Link>
                 Instagram
               </li>
               <li className="flex py-1 px-0 items-center">
-                <a href="/">
+                <Link href="/">
                   <FaTwitter size={15} className="mr-1" />
-                </a>
+                </Link>
                 Twitter
               </li>
               <li className="flex py-1 px-0 items-center">
-                <a href="/">
+                <Link href="/">
                   <HiOutlineMail size={15} className="mr-1" />
-                </a>{" "}
+                </Link>{" "}
                 Mail
               </li>
             </ul>
