8dfe9ea09a334b71825455f6fd1ae98dc305e574
rashodk <rmanamperiko@mun.ca>
2023-01-22 01:11:00 -0330

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import About from "../src/Components/About";
 import Work from "../src/Components/Work";
 import Contact from "../src/Components/Contact";
 import Footer from "../src/Components/Footer";
-import Navbar from "../src/Components/navbar";
+// import Navbar from "../src/Components/navbar";
 import Gallery from "../src/Components/Gallery";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <Navbar />
+      {/* <Navbar /> */}
       <Main />
       <About />
       <Work />
