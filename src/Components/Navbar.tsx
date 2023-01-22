f8a86567fef15001639a078f971b373c5cd2c7ca
rashodk <rmanamperiko@mun.ca>
2023-01-22 01:20:13 -0330

    renamed the class

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -11,7 +11,7 @@ import { HiOutlineMail } from "react-icons/hi";
 
 type Props = {};
 
-const Navbar = (props: Props) => {
+const NavBar = (props: Props) => {
   const [nav, setNav] = useState(false);
   const HandleClick = () => setNav(!nav);
 
@@ -77,4 +77,4 @@ const Navbar = (props: Props) => {
   );
 };
 
-export default Navbar;
+export default NavBar;
