396e7812485c7f7a3130a42e472acff6d24e45c6
rashodk <rmanamperiko@mun.ca>
2023-01-23 04:32:30 -0330

    added import

--- pages/src/Components/About.tsx
+++ pages/src/Components/About.tsx
@@ -3,6 +3,9 @@ import { HiArrowCircleLeft, HiArrowCircleRight } from "react-icons/hi";
 // import ArtistCv from "./subComponents/ArtistCv";
 
 import Image from "next/image";
+import Link from "next/link";
+
+
 type Props = {};
 
 const About = (props: Props) => {
@@ -29,30 +32,53 @@ const About = (props: Props) => {
               height={image.height}
             />
           </div>
-
           <p className="py-4 text-[12px] xsm:text-md xl:text-xl text-left font-light ">
             <strong className="font-bold">Bandu Manamperi</strong> is a renowned
             artist who holds a BFA in sculpture and is a part of Colombo&apos;s
-            Theertha Artists&apos; Collective. He is a pioneer of performance art
-            in Sri Lanka and continues to be an influential figure in the field.
-            He creates deeply personal artworks that focus on the transformation
-            of his own body, while also working in other mediums such as
-            sculpture, drawing, painting, and installation. He frequently
-            lectures and consults on various topics such as contemporary art,
-            performance, museology, and traditional craft techniques. His art
-            practice explores themes of memory and the ways in which external
-            events and ideologies shape our individual experiences and become
-            inscribed in our bodies.
+            Theertha Artists&apos; Collective. He is a pioneer of performance
+            art in Sri Lanka and continues to be an influential figure in the
+            field. He creates deeply personal artworks that focus on the
+            transformation of his own body, while also working in other mediums
+            such as sculpture, drawing, painting, and installation. He
+            frequently lectures and consults on various topics such as
+            contemporary art, performance, museology, and traditional craft
+            techniques. His art practice explores themes of memory and the ways
+            in which external events and ideologies shape our individual
+            experiences and become inscribed in our bodies.
           </p>
-          <a
-            href="https://photosbyrashod.myportfolio.com"
-            target="_blank"
-            rel="noreferrer"
+          {/* <Link
+            href="/artistcv"
             className="p-4 flex text-md hover:scale-110 transition-all duration-1000 ease-in-out bg-black w-[130px] text-white rounded-[45px] px-4 py-2 items-center justify-center space-x-2"
           >
             View CV
             <HiArrowCircleRight />
-          </a>
+          </Link> */}
+          <button
+            className="p-4 flex text-md hover:scale-110 transition-all duration-1000 ease-in-out bg-black w-[130px] text-white rounded-[45px] px-4 py-2 items-center justify-center space-x-2"
+            onClick={() => setIsPopupOpen(true)}
+          >
+            View CV
+            <HiArrowCircleRight />
+          </button>
+          {isPopupOpen && (
+            <div className="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+              <div className="absolute top-0 left-0 w-full h-full bg-white opacity-20"></div>
+              <div className="bg-white w-full md:max-w-full mx-auto rounded shadow-lg py-4 text-left px-6">
+                <div className="flex justify-between items-center pb-3 opacity-100">
+                  <h1 className="text-2xl font-bold text-black">
+                    Bandu Manamperi
+                  </h1>
+                  
+                  <div
+                    className="cursor-pointer z-50"
+                    onClick={() => setIsPopupOpen(false)}
+                  >
+                    <HiArrowCircleLeft />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
 
         {/* <button onClick={() => setIsPopupOpen(true)}>Open CV</button> */}
