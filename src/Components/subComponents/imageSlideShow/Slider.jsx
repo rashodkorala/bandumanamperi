15b7f189d6f4bdd106755fddd1791d941aa0ae68
rashodk <rmanamperiko@mun.ca>
2023-02-27 05:43:42 -0330

    resolve import error

--- src/Components/subComponents/imageSlideShow/Slider.jsx
+++ src/Components/subComponents/imageSlideShow/Slider.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
-import { RxDotFilled } from "react-icons/rx";
+import { BsDot } from "react-icons/bs";
 
 function Slider() {
   const slides = [
@@ -43,8 +43,6 @@ function Slider() {
   return (
     <div className="max-w-[1400px] h-[400px] md:h-[600px] xl:h-[780px] w-full m-auto py-16 px-4 relative group">
       <div
-        onDrag={prevSlide}
-        onDragEnd={nextSlide}
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
       {/* Left Arrow */}
@@ -58,7 +56,7 @@ function Slider() {
       <div className="flex top-4 justify-center py-2">
         {slides.map((slide, slideIndex) => (
           <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className="text-2xl cursor-pointer">
-            <RxDotFilled />
+            <BsDot />
           </div>
         ))}
       </div>
