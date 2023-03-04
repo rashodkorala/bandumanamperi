7f37e51bf933543657deb51ba807f4c7778271ae
rashodk <rmanamperiko@mun.ca>
2023-03-04 18:27:45 -0330

    add non repating components to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "../src/Components/Navbar";
+import Footer from "../src/Components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />;
+      <Footer />
+    </>
+  );
 }
