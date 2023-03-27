a46577167603e70a314012c2111abfe7d5df1ab3
rashodk <rmanamperiko@mun.ca>
2023-03-27 01:30:43 -0230

    remove unwanted imports

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Footer from "../src/Components/Footer";
 import Gallery from "../src/Components/Gallery";
-import Navbar from "../src/Components/Navbar";
 
 type Props = {};
 
