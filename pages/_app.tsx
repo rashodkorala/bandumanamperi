2dca829d7539d47308f6c551bf2b1b6212b30a4f
rashodk <rmanamperiko@mun.ca>
2023-12-08 17:14:35 -0330

    add website insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../src/Components/Navbar";
 import Footer from "../src/Components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+       <SpeedInsights />
       <Footer />
     </>
   );
