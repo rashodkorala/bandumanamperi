f53eb7e20e8f30e20857135a696a43bde87b5ac4
rashodk <rmanamperiko@mun.ca>
2024-03-08 17:00:34 -0330

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
-import Main from "../src/Components/Home";
-import About from "../src/Components/About";
-import Work from "../src/Components/Work";
-import Contact from "../src/Components/Contact";
-import Footer from "../src/Components/Footer";
-import Navbar from "../src/Components/navbar";
-import Gallery from "../src/Components/Gallery";
+import Main from "../src/Components/Main";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <Navbar />
+    <div>
       <Main />
-      <About />
-      <Work />
-      <Contact />
-      <Gallery />
-      <Footer />
     </div>
   );
 }
