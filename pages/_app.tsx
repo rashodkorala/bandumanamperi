919f0fc76b2af41de9d11c1995e91e3a0a1d4598
rashodk <rmanamperiko@mun.ca>
2024-03-09 17:47:48 -0330

    add baseurl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-
-import '../styles/globals.css';
+// _app.tsx
+import Footer from '@/src/components/Footer';
+import NavBar from '@/src/components/NavBar';
+import { ThemeProvider } from '@/src/components/ThemeContext';
+import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 
@@ -9,11 +11,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   const showFooter = router.pathname !== '/login';
   const showNavBar = router.pathname !== '/login';
   return (
-   
-   
+    <ThemeProvider>
+      {showNavBar && <NavBar />}
       <Component {...pageProps} />
-     
-   
+      {showFooter && <Footer />}
+    </ThemeProvider>
   );
 }
 
