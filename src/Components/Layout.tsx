e854a030ad89fecda7fb293cb498d0b7051e3a49
rashodk <rmanamperiko@mun.ca>
2024-03-13 05:33:24 -0230

    Refactor layout component and remove protectedPage component

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,40 +1,45 @@
-import { useState, useEffect } from 'react';
+// components/Layout.tsx
+
+import { useState, useEffect, use } from 'react';
 import { useRouter } from 'next/router';
-import { useAuthState } from 'react-firebase-hooks/auth';
+
 import { auth } from '@/utils/firebase-config';
+import { getAuth } from 'firebase/auth';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import { ThemeProvider } from './ThemeContext';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
-  const [user, loading, error] = useAuthState(auth);
   const [showLoading, setShowLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowLoading(false); // After 2 seconds, hide the loading screen
-    }, 1000);
+    }, 2000);
 
     return () => clearTimeout(timer); // Clear the timer when component unmounts
   }, []);
 
   useEffect(() => {
-    if (!loading) {
-      // If user is not logged in and tries to access protectedPage, redirect to auth page
-      if (!user && router.pathname === '/protectedPage') {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
         router.push('/auth');
       }
-    }
-  }, [loading, user, router]);
+    });
 
-  const showNavBar = router.pathname !== '/auth' && router.pathname !== '/protectedPage';
-  const showFooter = router.pathname !== '/auth' && router.pathname !== '/protectedPage';
+    return () => unsubscribe();
+  }
+  , [router]);
+
+  
+  const showNavBar = router.pathname !== '/login' && router.pathname !== '/protectedPage';
+  const showFooter = router.pathname !== '/login' && router.pathname !== '/protectedPage';
 
   if (showLoading) {
     return (
       <div className="flex items-center justify-center h-screen">
-        <div className="text-2xl font-bold">Loading...</div>
+        <p>Loading...</p>
       </div>
     );
   }
